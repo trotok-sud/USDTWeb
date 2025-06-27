ceae01dceefb1bfff14a87d9495833b306f4aa89
trotok-sud <217881025+trotok-sud@users.noreply.github.com>
2025-06-27 02:43:42 +0100

    s

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/USDTWeb/', // 👈 this is crucial for GitHub Pages
   plugins: [react()],
-})
+});
